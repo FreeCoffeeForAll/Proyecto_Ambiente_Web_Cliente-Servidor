66bec3f6e2bbdfa478850f9a069af0e009239549
Daniel Sánchez <lightningsound@outlook.com>
2022-11-20 20:02:28 -0600

    Cambios Base de Datos

--- DB/script.sql
+++ DB/script.sql
@@ -1,59 +1,261 @@
-CREATE TABLE `tienda`.`categoria` 
-( `id_categoria` INT(11) NOT NULL AUTO_INCREMENT , 
-`nom_categoria` VARCHAR(300) NOT NULL , 
-`desc_categoria` VARCHAR(250) NOT NULL , 
-PRIMARY KEY (`id_categoria`)) 
-ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-11-2022 a las 02:57:27
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
-CREATE TABLE `tienda`.`producto` 
-( `id_producto` INT(11) NOT NULL AUTO_INCREMENT , 
-`nom_producto` VARCHAR(300) NOT NULL , 
-`marca` VARCHAR(250) NOT NULL , 
-`precio` INT(11) NOT NULL , 
-`desc_producto` VARCHAR(250) NOT NULL , 
-`id_categoria` INT NOT NULL , 
-PRIMARY KEY (`id_producto`),
-FOREIGN KEY (`id_categoria`) REFERENCES `tienda`.`categoria`(`id_categoria`)) 
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `tienda`.`usuarios` 
-( `id_Usuario` INT(11) NOT NULL AUTO_INCREMENT , 
-`nomb1_User` VARCHAR(250) NOT NULL ,
-`nomb2_User` VARCHAR(250) NOT NULL ,
-`apel1_User` VARCHAR(250) NOT NULL ,
-`apel2_User` VARCHAR(250) NOT NULL ,
-`correo_User` VARCHAR(250) NOT NULL , 
-`telefono_User` INT(11) NOT NULL , 
-PRIMARY KEY (`id_Usuario`)) 
-ENGINE = InnoDB;
 
-CREATE TABLE `tienda`.`detalle_factura` 
-( `id_DetalleFact` INT(11) NOT NULL AUTO_INCREMENT , 
-`Id_Usuario` INT(11) NOT NULL , 
-`FechaHora` DATE NOT NULL , 
-`Total` FLOAT(11) NOT NULL , 
-PRIMARY KEY (`id_DetalleFact`),
-FOREIGN KEY (`id_Usuario`) REFERENCES `tienda`.`usuarios`(`id_Usuario`)) 
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `tienda`.`factura` 
-( `id_Factura` INT(11) NOT NULL AUTO_INCREMENT , 
-`id_Producto` INT(11) NOT NULL , 
-`id_DetalleFact` INT(11) NOT NULL , 
-`Total` INT(11) NOT NULL , 
-`Precio` FLOAT(11) NOT NULL , 
-PRIMARY KEY (`id_Factura`),
-FOREIGN KEY (`id_Producto`) REFERENCES `tienda`.`producto`(`id_producto`), 
-FOREIGN KEY (`id_DetalleFact`) REFERENCES `tienda`.`detalle_factura`(`id_DetalleFact`)) 
-ENGINE = InnoDB;
+--
+-- Base de datos: `tienda`
+--
 
-ALTER TABLE `producto` ADD `img` BLOB NOT NULL AFTER `id_categoria`;
-ALTER TABLE `producto` ADD `cantidad` INT(11) NOT NULL AFTER `precio`;
+-- --------------------------------------------------------
 
-INSERT INTO `categoria` (`id_categoria`, `nom_categoria`, `desc_categoria`) VALUES ('1', 'Mountain Bike', 'Bicicletas de montaña'), ('2', 'BMX', 'Bicicleta para acrobacias'), ('3', 'Ruta', 'Bicicletas diseñadas para la velocidad'), ('4', 'Niños', 'Biciceltas para niños');
+--
+-- Estructura de tabla para la tabla `categoria`
+--
 
-INSERT INTO `usuarios` (`id_Usuario`, `nom_User`, `contra`, `nombre_usuario`, `apel_User`, `correo_User`, `telefono_User`, `rol_id`) VALUES ('1', 'admin', 'admin', 'admin', 'admin', 'admin', '1223', '1');
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nom_categoria` varchar(300) NOT NULL,
+  `desc_categoria` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `roles` (`id`, `rol`) VALUES ('1', 'admin'), ('2', 'usuario');
+--
+-- Volcado de datos para la tabla `categoria`
+--
 
-INSERT INTO `usuarios` (`id_Usuario`, `nom_User`, `contra`, `nombre_usuario`, `apel_User`, `correo_User`, `telefono_User`, `rol_id`) VALUES ('2', 'Juan', '123', 'Juan', 'Lopez', 'juanito22@gmail.com', '123', '2');
+INSERT INTO `categoria` (`id_categoria`, `nom_categoria`, `desc_categoria`) VALUES
+(1, 'Mountain Bike', 'Bicicletas de montaña'),
+(2, 'BMX', 'Bicicleta para acrobacias'),
+(3, 'Ruta', 'Bicicletas diseñadas para la velocidad'),
+(4, 'Niños', 'Biciceltas para niños');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_factura`
+--
+
+CREATE TABLE `detalle_factura` (
+  `id_DetalleFact` int(11) NOT NULL,
+  `Id_Usuario` int(11) NOT NULL,
+  `FechaHora` date NOT NULL,
+  `Total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id_Factura` int(11) NOT NULL,
+  `id_Producto` int(11) NOT NULL,
+  `id_DetalleFact` int(11) NOT NULL,
+  `Total` int(11) NOT NULL,
+  `Precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `nom_producto` varchar(300) NOT NULL,
+  `marca` varchar(250) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `desc_producto` varchar(250) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `img` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `nom_producto`, `marca`, `precio`, `cantidad`, `desc_producto`, `id_categoria`, `img`) VALUES
+(17, 'Bicicleta de Montaña - NA', 'SUPERFIT', 99, 99, 'Bicicleta de montaña de color amarillo y negro   ', 1, '71HtpU2prpL._AC_SX425_.jpg'),
+(18, 'Bicicleta de Montaña - AN', 'SUPERFIT', 105, 19, 'Bicicleta de Montaña de color azul con naranja', 1, 'descarga.jfif'),
+(19, 'Bicicleta BMX - M', 'Mongoose', 60, 56, 'Bicicleta BMX Morada', 2, '51oVBGq0x1L._SL500_.jpg'),
+(20, 'Bicicleta de Niños', 'YAMAHA', 30, 50, 'Bicicleta de Niños', 4, 'D_NQ_NP_761024-MLV44201395200_112020-O.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(2) NOT NULL,
+  `rol` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `rol`) VALUES
+(1, 'admin'),
+(2, 'usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_Usuario` int(11) NOT NULL,
+  `nom_User` varchar(20) NOT NULL,
+  `contra` varchar(20) NOT NULL,
+  `nombre_usuario` varchar(20) NOT NULL,
+  `apel_User` varchar(50) NOT NULL,
+  `correo_User` varchar(50) NOT NULL,
+  `telefono_User` int(11) NOT NULL,
+  `rol_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_Usuario`, `nom_User`, `contra`, `nombre_usuario`, `apel_User`, `correo_User`, `telefono_User`, `rol_id`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin', 'admin', 1223, 1),
+(2, 'Juan', '123', 'Juan', 'Lopez', 'juanito22@gmail.com', 123, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  ADD PRIMARY KEY (`id_DetalleFact`),
+  ADD KEY `Id_Usuario` (`Id_Usuario`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id_Factura`),
+  ADD KEY `id_Producto` (`id_Producto`),
+  ADD KEY `id_DetalleFact` (`id_DetalleFact`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_Usuario`),
+  ADD KEY `rol_id` (`rol_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  MODIFY `id_DetalleFact` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id_Factura` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  ADD CONSTRAINT `detalle_factura_ibfk_1` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuarios` (`id_Usuario`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`id_Producto`) REFERENCES `producto` (`id_producto`),
+  ADD CONSTRAINT `factura_ibfk_2` FOREIGN KEY (`id_DetalleFact`) REFERENCES `detalle_factura` (`id_DetalleFact`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
